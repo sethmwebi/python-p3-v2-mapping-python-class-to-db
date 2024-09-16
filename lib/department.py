ce2ae49fddddc842f74539ae1f06a29e332273d6
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-16 09:50:28 +0300

    department crud

--- lib/department.py
+++ lib/department.py
@@ -2,39 +2,31 @@ from __init__ import CONN, CURSOR
 
 
 class Department:
-
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
         self.location = location
 
     def __repr__(self):
-        return f"<Department {self.id}: {self.name}, {self.location}>"
+        return f"<Department {self.id}: {self.name}: {self.location}>"
 
     @classmethod
     def create_table(cls):
-        """Create a new table to persist the attributes of Department instances"""
-        sql = """
-            CREATE TABLE IF NOT EXISTS departments (
-                id INTEGER PRIMARY KEY,
-                name TEXT,
-                location TEXT
-            )
-        """
+        """Create a new table to persist the attributes of department instances."""
+        sql = """CREATE TABLE IF NOT EXISTS departments (id INTEGER PRIMARY KEY, name TEXT, location TEXT)"""
         CURSOR.execute(sql)
         CONN.commit()
 
     @classmethod
     def drop_table(cls):
-        """Drop the table that persists Department instances"""
-        sql = """
-            DROP TABLE IF EXISTS departments
-        """
+        """Drop the table that persists department instances"""
+        sql = """DROP TABLE IF EXISTS departments"""
         CURSOR.execute(sql)
         CONN.commit()
 
     def save(self):
-        """Insert a new row with the name and location values of the current Department instance. Update object id attribute using the primary key value of a new row"""
+        """Insert a new row with the name and location values of the current Department instance
+        Update object id attribute using the primary key value of new row"""
         sql = """INSERT INTO departments (name, location) VALUES (?, ?)"""
         CURSOR.execute(sql, (self.name, self.location))
         CONN.commit()
@@ -43,13 +35,13 @@ class Department:
 
     @classmethod
     def create(cls, name, location):
-        """Initialize a new department instance and save the object to the database"""
+        """Initialize a new Department instance and save the object to the database"""
         department = cls(name, location)
         department.save()
         return department
 
     def update(self):
-        """Update the table row corresponding to the current database instance."""
+        """Update the table row corresponding to the current Department instance"""
         sql = """UPDATE departments SET name = ?, location = ? WHERE id = ?"""
         CURSOR.execute(sql, (self.name, self.location, self.id))
         CONN.commit()
